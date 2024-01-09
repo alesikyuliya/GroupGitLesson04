b88d109752fdd021ef44b34636e91689adca84c7
AndrewAlesik <olesik.andrei@gmail.com>
2024-01-09 19:42:03 +0100

    add new system out println

--- src/Lesson4GitProgram.java
+++ src/Lesson4GitProgram.java
@@ -2,7 +2,7 @@ public class Lesson4GitProgram {
 
     public static void main(String[] args){
         System.out.println("Hello world from Julia to Git");
-
+        System.out.println("Second commit to Git");
 
     }
 
